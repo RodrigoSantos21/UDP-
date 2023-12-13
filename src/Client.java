83db92976f04bb621bb3c0f9def4b541c6076bb0
RodrigoSantos21 <rodrigoaugusto2103@gmail.com>
2023-12-13 09:43:53 -0300

    UDP funcionando com a manipulação de perda de arquivo

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.Objects;
 import java.util.Scanner;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,6 +20,12 @@ public class Client {
         byte[] senddata, receivedata;
         String nomeArquivo = "";
         InetAddress address = InetAddress.getLocalHost();
+        String ackArquivo = "";
+        String hash = "";
+        String hashOriginal = "";
+        String rawData = "";
+        String serverResponse = "";
+        Boolean perdeuPacote = false;
 
         // envia o endereço e porta do cliente para o servidor
         String ola = "Endereço:" + address + ", Porta:" + port;
@@ -28,28 +38,98 @@ public class Client {
             receivedata = new byte[10000];
             indata = new DatagramPacket(receivedata, receivedata.length);
             ds.receive(indata);
-            String rawData = new String(indata.getData());
-            String serverResponse = rawData.substring(0, (indata.getLength()));
+            rawData = new String(indata.getData());
+            serverResponse = rawData.substring(0, (indata.getLength()));
             System.out.println(serverResponse);
+
+            if (serverResponse.contains("Tamanho")){
+                String[] infos = (serverResponse.split(" "));
+                // System.out.println(Arrays.toString(infos));
+                int tamanhoArquivo = (int) Long.parseLong(infos[1]);
+                int tamanhoPacote = Integer.parseInt(infos[5]);
+                int tamanhoCabecalho = Integer.parseInt(infos[10]);
+                int numeroPacotes = 0;
+                int destPos = 0;
+                // TODO: tratar separadamente o envio dos pacotes
+                receivedata = new byte[tamanhoPacote];
+                byte [] fileComplete = new byte[(int) tamanhoArquivo];
+                while(tamanhoArquivo > 0) {
+                    indata = new DatagramPacket(receivedata, receivedata.length);
+                    ds.receive(indata);
+                    String cabecalho = new String(indata.getData()).substring(0,tamanhoCabecalho);
+                    System.out.println(cabecalho);
+                    if (cabecalho.contains("1") && !perdeuPacote){
+                        ackArquivo = "";
+                        perdeuPacote = true;
+                    }
+                    else{
+                        System.arraycopy(indata.getData(), tamanhoCabecalho, fileComplete, destPos, (Math.min(tamanhoArquivo, (tamanhoPacote - tamanhoCabecalho))));
+                        ackArquivo = "ACK " + numeroPacotes;
+                        numeroPacotes++;
+                        destPos += tamanhoPacote - tamanhoCabecalho;
+                        tamanhoArquivo -= tamanhoPacote - tamanhoCabecalho;
+                    }
+                    senddata = ackArquivo.getBytes();
+                    outdata = new DatagramPacket(senddata, senddata.length, address, port);
+                    ds.send(outdata);
+                }
+                Path path = Paths.get("C:/Users/Rodrigo/Downloads/UDP-/novoArquivo4.png");
+                Files.write(path, fileComplete);
+                hash = stringHexa(Objects.requireNonNull(gerarHash(Arrays.toString(fileComplete), "SHA-256")));
+            }
+
+            if (serverResponse.contains("não existe")){
+                nomeArquivo = "";
+            }
+
+            if(serverResponse.contains("Checksum")){
+                String[] checksum = serverResponse.split(" ");
+                hashOriginal = checksum[1];
+                System.out.println(hashOriginal);
+                System.out.println(hash);
+                if(hashOriginal.equalsIgnoreCase(hash)){
+                    System.out.println("Arquivo igual!");
+                }
+                else{
+                    System.out.println("Houve perdas");
+                }
+                nomeArquivo = "";
+            }
+
+            if (nomeArquivo.equalsIgnoreCase("sair")){
+                break;
+            }
 
             if (nomeArquivo.isEmpty()){
                 // envia o nome do arquivo
-                System.out.println("Insira a palavra Arquivo e logo em seguida, o nome do arquivo ou digite 'a' para sair:");
+                System.out.println("Insira a palavra Arquivo e logo em seguida, o nome do arquivo ou digite 'Sair' para sair:");
                 nomeArquivo = sc.nextLine();
                 senddata = nomeArquivo.getBytes();
                 outdata = new DatagramPacket(senddata, senddata.length, address, port);
                 ds.send(outdata);
             }
-
-            if (serverResponse.contains("Tamanho")){
-                String[] infos = (serverResponse.split(" "));
-                System.out.println(infos[1]);
-            }
-
-            if (nomeArquivo.equalsIgnoreCase("a")){
-                break;
-            }
         }
         ds.close();
+    }
+
+    public static byte[] gerarHash(String frase, String algoritmo) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algoritmo);
+            md.update(frase.getBytes());
+            return md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    private static String stringHexa(byte[] bytes) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            int parteAlta = ((bytes[i] >> 4) & 0xf) << 4;
+            int parteBaixa = bytes[i] & 0xf;
+            if (parteAlta == 0) s.append('0');
+            s.append(Integer.toHexString(parteAlta | parteBaixa));
+        }
+        return s.toString();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,17 +3,32 @@ import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Server {
+    static int port = 1235;
+    static byte [] senddata;
+    static byte [] receivedata;
+    static DatagramPacket outdata = null;
+    static DatagramPacket indata = null;
+    static DatagramSocket socket;
+
+    static {
+        try {
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static String diretorio = "C:/Users/Rodrigo/Downloads/";
+    static boolean isConnected = false;
     public static void main(String[] args) throws IOException, InterruptedException {
-        int port = 1235;
-        byte [] senddata, receivedata;
-        DatagramPacket outdata = null;
-        DatagramPacket indata = null;
-        DatagramSocket socket = new DatagramSocket(port);
-        String diretorio = "C:/Users/Rodrigo/Downloads/";
-        boolean isConnected = false;
+        String hash = "";
+        String resposta = "";
         while (true){
             receivedata = new byte[10000];
             indata = new DatagramPacket(receivedata, receivedata.length);
@@ -22,30 +37,110 @@ public class Server {
             int clientPort = indata.getPort();
             String rawData = new String(indata.getData());
             String clientData = rawData.substring(0, (indata.getLength()));
+            System.out.println(clientData);
 
-            if (!isConnected){
-                String resposta = "Olá cliente!";
+            if (clientData.contains("Endereço")){
+                resposta = "Olá cliente!";
                 senddata = resposta.getBytes();
                 outdata = new DatagramPacket(senddata, 0, senddata.length, clientAddress, clientPort);
                 socket.send(outdata);
-                isConnected = true;
             }
 
             if(clientData.contains("Arquivo")) {
                 // TODO: transformar em função
-                String[] nomeArquivo = (clientData.split(" "));
+                String[] nomeArquivo = clientData.split(" ", 2);
                 diretorio = diretorio.concat(nomeArquivo[1]);
+                File f = new File(diretorio);
+                if(!f.exists()) {
+                    resposta = "Arquivo não existe, digite um arquivo válido!";
+                    System.out.println(resposta.length());
+                    senddata = resposta.getBytes();
+                    outdata = new DatagramPacket(senddata, senddata.length, clientAddress, clientPort);
+                    socket.send(outdata);
+                    continue;
+                }
                 final Path path = Paths.get(diretorio);
-                long tamanhoArquivo = Files.size(path);
-                String infosArquivo = "Tamanho: " + tamanhoArquivo + " KB e Pacotes: 10 KB";
-                byte[] bytes = new byte[10000];
-                bytes = infosArquivo.getBytes();
-                senddata = bytes;
+                int numeroPacotes = 0;
+                int srcPos = 0;
+                int tamanhoArquivo = (int) Files.size(path);
+                String infosArquivo2 = "Pacote " + numeroPacotes;
+                int tamanhoPacote = 10000 + infosArquivo2.length();
+                String infosArquivo = "Tamanho: " + tamanhoArquivo + " bytes e Pacotes: " + tamanhoPacote + " bytes com cabeçalho de " + infosArquivo2.length() + " bytes";
+                senddata = infosArquivo.getBytes();
                 outdata = new DatagramPacket(senddata, senddata.length, clientAddress, clientPort);
                 socket.send(outdata);
+                byte[] bytes = new byte[tamanhoPacote];
+                try {
+                    // arquivo para bytes
+                    senddata = bytes;
+                    bytes = Files.readAllBytes(path);
+                    while(tamanhoArquivo > 0){
+                        System.arraycopy(infosArquivo2.getBytes(), 0, senddata, 0, (infosArquivo2.length()));
+                        System.arraycopy(bytes, srcPos, senddata, infosArquivo2.length(), (Math.min(tamanhoArquivo, (tamanhoPacote - infosArquivo2.length()))));
+                        outdata = new DatagramPacket(senddata, senddata.length, clientAddress, clientPort);
+                        socket.send(outdata);
+                        try { Thread.sleep (10000); } catch (InterruptedException ex) {}
+                        receivedata = new byte[10000];
+                        indata = new DatagramPacket(receivedata, receivedata.length);
+                        socket.receive(indata);
+                        String rawACK = new String(indata.getData());
+                        String clientACK = rawACK.substring(0, (indata.getLength()));
+                        System.out.println(clientACK);
+                        if (!clientACK.contains("ACK")) {
+                            while (!clientACK.contains("ACK")) {
+                                System.arraycopy(infosArquivo2.getBytes(), 0, senddata, 0, (infosArquivo2.length()));
+                                System.arraycopy(bytes, srcPos, senddata, infosArquivo2.length(), (Math.min(tamanhoArquivo, (tamanhoPacote - infosArquivo2.length()))));
+                                outdata = new DatagramPacket(senddata, senddata.length, clientAddress, clientPort);
+                                socket.send(outdata);
+                                try {
+                                    Thread.sleep(10000);
+                                } catch (InterruptedException ex) {
+                                }
+                                receivedata = new byte[10000];
+                                indata = new DatagramPacket(receivedata, receivedata.length);
+                                socket.receive(indata);
+                                rawACK = new String(indata.getData());
+                                clientACK = rawACK.substring(0, (indata.getLength()));
+                                System.out.println(clientACK);
+                            }
+                        }
+                        numeroPacotes++;
+                        infosArquivo2 = "Pacote " + numeroPacotes;
+                        srcPos += tamanhoPacote - infosArquivo2.length();
+                        tamanhoArquivo -= tamanhoPacote - infosArquivo2.length();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 diretorio = "C:/Users/Rodrigo/Downloads/";
+                hash = stringHexa(Objects.requireNonNull(gerarHash(Arrays.toString(bytes), "SHA-256")));
+                infosArquivo = "Checksum: " + hash;
+                senddata = infosArquivo.getBytes();
+                outdata = new DatagramPacket(senddata, senddata.length, clientAddress, clientPort);
+                socket.send(outdata);
             }
         }
 
+    }
+
+    public static byte[] gerarHash(String frase, String algoritmo) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algoritmo);
+            md.update(frase.getBytes());
+            return md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    private static String stringHexa(byte[] bytes) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            int parteAlta = ((bytes[i] >> 4) & 0xf) << 4;
+            int parteBaixa = bytes[i] & 0xf;
+            if (parteAlta == 0) s.append('0');
+            s.append(Integer.toHexString(parteAlta | parteBaixa));
+        }
+        return s.toString();
     }
 }
